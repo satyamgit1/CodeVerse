d0b4849003fb8468c96d38e1de38ad016d32928c
Satyam singh <satyamsingh2003a@gmail.com>
2024-08-28 15:39:38 +0530

    git env for firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,31 +1,4 @@
-// // firebaseConfig.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage"; 
 
-// // Your Firebase configuration object
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB3cD94lYK3OlwCXNlvRmCAUcQBad-kMvI",
-//     authDomain: "codeeditor-7b515.firebaseapp.com",
-//     projectId: "codeeditor-7b515",
-//     storageBucket: "codeeditor-7b515.appspot.com",
-//     messagingSenderId: "687839287286",
-//     appId: "1:687839287286:web:58b817dc47b1149c6ad9d7",
-//     measurementId: "G-C8P14Z5DK8"
-// };
-
-// // Initialize Firebase app
-// const app = initializeApp(firebaseConfig);
-
-// // Initialize Firebase services
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-// const storage = getStorage(app);
-// const provider = new GoogleAuthProvider();
-
-// // Export Firebase services and Google Auth Provider
-// export { auth, db, storage, provider };
 
 
 import { initializeApp } from "firebase/app";
